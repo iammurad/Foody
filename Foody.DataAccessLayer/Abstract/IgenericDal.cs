c6fe351084212dc7fb505e65386640648a4e5af2
iammurad <murad.theynalli@gmail.com>
2025-05-16 21:18:52 +0200

    Added Generic Interface

---
+++ Foody.DataAccessLayer/Abstract/IgenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Abstract
+{
+    public interface IgenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        List<T> GetAll();
+    }
+    
+
+}
