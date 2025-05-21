bc5084eeb439217ee9b1eed3735903d04fcf0c23
iammurad <murad.theynalli@gmail.com>
2025-05-22 01:11:44 +0200

    Refactor layout using ASP.NET Core View Components
    This commit refactors the HTML structure of `Index.cshtml` and `Default.cshtml` by replacing static content with ASP.NET Core View Components. Key sections such as `<head>`, `<sidebar>`, `<main panel>`, and `<footer>` have been modularized into their respective components, enhancing code maintainability and reusability. New View Component classes have been created to encapsulate rendering logic, while existing JavaScript and CSS includes remain intact to ensure proper functionality. Overall, these changes improve the codebase's structure for easier management and future extensions.

---
+++ Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_FooterlLayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿// Ignore Spelling: Foody
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _FooterLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_SidebarLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _SidebarLayoutComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
