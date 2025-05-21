abfdbb267d09771278d48a721c7145119905f83d
iammurad <murad.theynalli@gmail.com>
2025-05-22 01:28:58 +0200

    Add services and configure database for Foody app
    This commit introduces several using directives for the Foody application, including business and data access layers. It initializes the web application builder, configures the database context, sets up controllers with views, and registers services for category data access and management, enhancing the application's structure and functionality.

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -1,7 +1,18 @@
+using Foody.BusinessLayer.Abstract;
+using Foody.BusinessLayer.Concrete;
+using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Context;
+using Foody.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<FoodyContext>();
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
 var app = builder.Build();
 
