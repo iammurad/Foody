0bae933c7bf93688815c24b2df413cd3b3bf09b5
iammurad <murad.theynalli@gmail.com>
2025-05-24 23:19:27 +0200

    Add product management features to ProductsController
    - Updated ProductsController to include ICategoryService for category management.
    - Added action methods: ProductDetails, CreateProduct, ProductUpdate, and DeleteProducts.
    - Modified ProductListWithCategories to include category data.
    - Fixed link in ProductListWithCategories.cshtml to point to CreateProduct action.
    - Created new CreateProduct.cshtml view with a form for adding new products.

--- Foody.PresentationLayer/Controllers/ProductsController.cs
+++ Foody.PresentationLayer/Controllers/ProductsController.cs
@@ -1,15 +1,19 @@
 ﻿using Foody.BusinessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Foody.PresentationLayer.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult ProductList()
@@ -22,5 +26,40 @@ namespace Foody.PresentationLayer.Controllers
             var products = _productService.TGetProductsWithCategory();
             return View(products);
         }
+
+        public IActionResult ProductDetails(int id)
+        {
+            var product = _productService.TGetById(id);
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            ViewBag.Categories = new SelectList(_categoryService.TGetAll(), "CategoryId", "CategoryName"); 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _productService.TInsert(product);
+            return RedirectToAction("ProductListWithCategories");
+        }
+        public IActionResult DeleteProducts(int id)
+        {
+            _productService.TDelete(id);
+            return RedirectToAction("ProductListWithCategories");
+        }
+        public IActionResult ProductUpdate(int id)
+        {
+            var product = _productService.TGetById(id);
+            return View(product);
+        }
+        [HttpPost]  
+        public IActionResult ProductUpdate(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("ProductListWithCategories");
+        }
+
     }
 }
