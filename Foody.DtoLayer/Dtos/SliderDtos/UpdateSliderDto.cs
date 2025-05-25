5ae2eaf3b4815d8b8dc3e533d05debbb3dad7d5e
iammurad <murad.theynalli@gmail.com>
2025-05-25 15:14:22 +0200

    Add DTOs for About and Slider; update project dependencies
    Updated `Foody.PresentationLayer.csproj` to include
    `AutoMapper` version `14.0.0` and existing package references.
    
    Created new Data Transfer Objects (DTOs) for the "About"
    section: `CreateAboutDto`, `GetByIdAboutDto`,
    `ResultAboutDto`, and `UpdateAboutDto`.
    
    Added new DTOs for the "Slider" section:
    `CreateSliderDto`, `GetByIdSliderDto`,
    `ResultSliderDto`, and `UpdateSliderDto`.

---
+++ Foody.DtoLayer/Dtos/SliderDtos/UpdateSliderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DtoLayer.Dtos.SliderDtos
+{
+    public class UpdateSliderDto
+    {
+        public int SliderId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
