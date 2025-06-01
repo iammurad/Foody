33b436e6f7c23737c6c0e2bc937126c3788c6611
iammurad <murad.theynalli@gmail.com>
2025-06-01 11:48:40 +0200

    Update routing and implement DefaultController with views
    - Changed default controller from Home to Default in Program.cs.
    - Added DefaultController with Index action in DefaultController.cs.
    - Created multiple view components for modular UI in the presentation layer.
    - Introduced Index.cshtml as the main view, integrating various components.
    - Enhanced Default.cshtml with structured layout, navigation, and carousel.
    - Improved HTML structure and styling, including meta tags and stylesheets.
    - Organized application structure for better modularity and maintainability.

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultAboutSectionComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultAboutSectionComponentPartial:ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public _DefaultAboutSectionComponentPartial(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
