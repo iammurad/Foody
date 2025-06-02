d773a447e166498e9047d401003d449269445ad3
iammurad <murad.theynalli@gmail.com>
2025-06-02 11:43:10 +0200

    Refactor product loading and display logic
    - Introduced `IProductService` dependency in `DefaultController` for fetching products by category.
    - Replaced direct view component call with a partial view `_ProductItemsPartial` for rendering products.
    - Removed main JavaScript file reference in `Index.cshtml` and jQuery script for category button clicks.
    - Created `_ProductItemsPartial.cshtml` to encapsulate product display logic and handle loading states via AJAX.
    - Improved maintainability and user experience with dynamic content loading.

--- Foody.PresentationLayer/Controllers/DefaultController.cs
+++ Foody.PresentationLayer/Controllers/DefaultController.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Foody.PresentationLayer.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IProductService _productService;
+
+        public DefaultController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        //public IActionResult LoadProductsByCategory(int categoryId)
+        //{
+        //    return ViewComponent("_DefaultProductSectionComponentPartial", new { categoryId=categoryId });
+        //}
+
         public IActionResult LoadProductsByCategory(int categoryId)
         {
-            return ViewComponent("_DefaultProductSectionComponentPartial", new { categoryId=categoryId });
+            var products = categoryId == 0
+                ? _productService.TGetProductsWithCategoryAndLast12Items()
+                : _productService.TGetProductsByCategory(categoryId);
+
+            return PartialView("_ProductItemsPartial", products); 
         }
     }
 }
